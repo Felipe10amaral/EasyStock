a78ceae67244b58b5336e955f2e1a7f0a876e6ba
Felipe Amaral <felipe.sf2011@gmail.com>
2022-03-22 23:40:40 -0300

    classe Product com responsabilidade de criar o produto

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,16 +1,23 @@
 import { Router } from "express";
+import {v4 } from 'uuid';
+import { Product } from "../models/Product";
 
 const Routes = Router();
 
-const product = [];
+const product :Product[]= [];
 
 Routes.post("/register", (request, response) => {
     const {model, quantity} = request.body;
 
-    product.push({
+    const addProduct = new Product();
+
+    Object.assign(addProduct, {
         model,
-        quantity
-    });
+        quantity,
+        createt_at: new Date()
+    })
+
+    product.push(addProduct)
 
     console.log(product);
     return response.status(201).send();
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Routes } from '../routes/routes';
+import { Routes } from './routes/productRoutes';
 
 const app = express();
 
