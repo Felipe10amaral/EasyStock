369b3174d78dbb049c10c471327d926642e0e552
Felipe Amaral <felipe.sf2011@gmail.com>
2022-04-01 22:34:22 -0300

    criando upload de arquivos com o multer

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,19 +1,30 @@
 import { Router } from "express";
+import multer from 'multer';
+
 import { listController } from "../useCases/listProduct";
 import { createProductController } from "../useCases/CreateProduct/index";
 
-
 const productRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp"
+});
 
 
 productRoutes.post("/", (request, response) => {
   return createProductController.handle(request, response);
-
 })
 
 productRoutes.get("/", (request, response) => {
   return listController.handle(request, response);
 })
+
+productRoutes.post("/import", upload.single("file") ,(request, response) => {
+  const {file} = request;
+  console.log(file);
+  return response.status(200).send();
+})
+
 
 
 export {productRoutes}
